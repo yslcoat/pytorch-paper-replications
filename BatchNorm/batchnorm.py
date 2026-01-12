a4bafd2de1ed9ab783e6860e8b25211de764a296
YSLCoat <torstein.forseth@outlook.com>
2026-01-12 22:47:22 +0100

    batchnorm2d

--- BatchNorm/batchnorm.py
+++ BatchNorm/batchnorm.py
@@ -17,6 +17,7 @@ class BatchNorm(nn.Module):
         self.register_buffer('running_var', torch.ones(n_features))
 
     def forward(self, x: torch.Tensor):
+        # x.shape: [B, C]
         if self.training:
             batch_mean = torch.mean(x, dim=0)
             batch_var = torch.var(x, dim=0, unbiased=False)
@@ -33,5 +34,41 @@ class BatchNorm(nn.Module):
 
         x_hat = (x - mean) / torch.sqrt(var + self.eps)
         scaled_and_shifted = self.gamma * x_hat + self.beta
+
+        return scaled_and_shifted
+    
+
+class BatchNorm2D(nn.Module):
+    def __init__(self, n_channels: int, eps: float = 1e-5, momentum: float = 0.1):
+        super().__init__()
+        self.n_channels = n_channels
+        self.eps = eps
+        self.momentum = momentum
+
+        self.gamma = nn.Parameter(torch.ones(self.n_channels))
+        self.beta = nn.Parameter(torch.zeros(self.n_channels))
+
+        # Used to calculate EMA instead of calculating mean and variance of the entire training set after training. 
+        self.register_buffer('running_mean', torch.zeros(n_channels))
+        self.register_buffer('running_var', torch.ones(n_channels))
+
+    def forward(self, x: torch.Tensor):
+        # x.shape: [B, C, H, W]
+        if self.training:
+            batch_mean = torch.mean(x, dim=(0, 2, 3), keepdim=True)
+            batch_var = torch.var(x, dim=(0, 2, 3), unbiased=False, keepdim=True)
+
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * batch_mean.squeeze()
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * batch_var.squeeze()
+
+            mean = batch_mean
+            var = batch_var
+        else:
+            mean = self.running_mean.view(1, self.n_features, 1, 1)
+            var = self.running_var.view(1, self.n_features, 1, 1)
+
+        x_hat = (x - mean) / torch.sqrt(var + self.eps)
+        scaled_and_shifted = self.gamma.view(1, self.n_channels, 1, 1) * x_hat + self.beta.view(1, self.n_channels, 1, 1)
 
         return scaled_and_shifted
